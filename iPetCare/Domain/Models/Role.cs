5446e420bf7b6460399382fb2a55af9106ba4b86
Paweł Kobryński <pawelkobrynski97@gmail.com>
2020-03-11 21:00:38 +0100

    Added Db Migartions, added Role.cs in Domain\Models

---
+++ iPetCare/Domain/Models/Role.cs
@@ -0,0 +1,9 @@
+﻿namespace Domain.Models
+{
+    public static class Role
+    {
+        public const string Owner = "Owner";
+        public const string Vet = "Vet";
+        public const string Administrator = "Administrator";
+    }
+}
